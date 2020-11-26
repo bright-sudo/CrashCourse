3975ef2dc43298f2040563edd31e4c2525cc4391
unknown <sahay.ohikc@gmail.com>
2020-11-26 23:34:43 +0200

    Rewritten eks, added simple jenkins pipeline and datadog agent container

---
+++ Oleksandr_Khabarov/Demo/variables.tf
@@ -0,0 +1,20 @@
+variable "image_name" {
+  description = "Image for container"
+  default     = "wernight/cpuminer-multi:alpine"
+}
+
+variable "wallet" {
+  description = "Wallet credentials"
+  default     = "DUSVnzc6zWbrWqrRVaZGC7pkT519Lwvmbs"
+}
+
+variable "algo" {
+  default = "qubit"
+}
+
+variable "url" {
+  default = "stratum+tcp://52.11.201.217:3012"
+}
+
+variable "datadog_api_key" {}
+variable "datadog_app_key" {}
